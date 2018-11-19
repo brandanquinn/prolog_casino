3a05c4450c6d78d3a8d83e78c8d7e291109caa72
Brandan Quinn <bquinn2@ramapo.edu>
2018-11-19 23:41:54 +0000

    Total Hours: 6; Fixed getting var from state rules. Board view clearly displayed, working on wiring up basic move making.

--- program.pl
+++ program.pl
@@ -64,14 +64,6 @@ card(c, k).
 card(c, a).
 
 /**
-Function Name: startGame
-Purpose: Begins the game for user; needs to be expanded upon later.
-Parameters:
-Algorithm: Simply prints a message to the screen.
-**/
-startGame() :- write("Game is starting.").
-
-/**
 Function Name: deck
 Purpose: Generate a Deck List
 Parameters: Takes an uninstantiated variable and assigns to it the pre-built deck list.
@@ -83,10 +75,6 @@ deck([
     (c, 2), (c, 2), (c, 3), (c, 4), (c, 5), (c, 6), (c, 7), (c, 8), (c, 9), (c, x), (c, j), (c, q), (c, k), (c, a)
 ]).
 
-% Game Players
-player("Human").
-player("Computer").
-
 /**
 Function Name: shuffledeck
 Purpose: Shuffle a Deck List and assign it to a variable
@@ -95,8 +83,8 @@ Algorithm:
     1. Assign pre-built deck list to Deck variable
     2. Call random_permutation() to shuffle pre-built deck list and assign the new list to ShuffledDeck variable 
 **/
-shuffleDeck(GameDeck) :-    deck(NewDeck),
-                            random_permutation(NewDeck, GameDeck).
+shuffleDeck(NewGameDeck, GameDeckBeforeMove) :- deck(NewGameDeck),
+                            random_permutation(NewGameDeck, GameDeckBeforeMove).
 
 /**
 Function Name: printCards
@@ -114,7 +102,7 @@ printCards([Top | Rest]) :- Top = (Suit, Type),
                             write(Type),
                             write(" "),
                             printCards(Rest).
-
+printCards(_) :- write("Printing cards from uninstantiated variable."), nl.
 /**
 Function Name: draw
 Purpose: Draw a Card from the top of the deck.
@@ -124,6 +112,11 @@ Algorithm: Pulls the top card from the Deck List and assigns it to the Card vari
 draw(Card, GameDeck, NewGameDeck) :- GameDeck = [Card | Rest],
                                     NewGameDeck = Rest.
 
+/**  [RoundNum, GameDeck, HumanScore, HumanHand, HumanPile, ComputerScore, ComputerHand, ComputerPile, Builds, TableCards, NextPlayer] **/
+
+getRoundNumFromState(State, _) :- State = [].
+getRoundNumFromState(State, RoundNum) :- nth0(0, State, RoundNum).
+
 /**
 Function Name: getDeckFromState
 Purpose: Pulls the GameDeck from the State List
@@ -132,7 +125,10 @@ Parameters:
     GameDeck, Variable to be instantiated to GameDeck from State.
 **/
 getDeckFromState(State, _) :- State = [].
-getDeckFromState(State, GameDeck) :- [GameDeck | _] = State.
+getDeckFromState(State, NewGameDeck) :- nth0(1, State, NewGameDeck).
+
+getHumanScoreFromState(State, _) :- State = [].                   
+getHumanScoreFromState(State, HumanScore) :- nth0(2, State, HumanScore).
 
 /**
 Function Name: getHumanHandFromState
@@ -142,7 +138,13 @@ Parameters:
     HumanHand, Variable to be instantiated to HumanHand from State.
 **/
 getHumanHandFromState(State, _) :- State = [].
-getHumanHandFromState(State, HumanHand) :- [HumanHand | _] = State.
+getHumanHandFromState(State, HumanHand) :- nth0(3, State, HumanHand).
+
+getHumanPileFromState(State, _) :- State = [].
+getHumanPileFromState(State, HumanPile) :- nth0(4, State, HumanPile).
+
+getComputerScoreFromState(State, _) :- State = [].
+getComputerScoreFromState(State, ComputerScore) :- nth0(5, State, ComputerScore).
 
 /**
 Function Name: getComputerHandFromState
@@ -152,7 +154,67 @@ Parameters:
     ComputerHand, Variable to be instantiated to ComputerHand from State.
 **/
 getComputerHandFromState(State, _) :- State = [].
-getComputerHandFromState(State, ComputerHand) :- [ComputerHand | _] = State.
+getComputerHandFromState(State, ComputerHand) :- nth0(6, State, ComputerHand).
+
+getComputerPileFromState(State, _) :- State = [].
+getComputerPileFromState(State, ComputerPile) :- nth0(7, State, ComputerPile).
+
+getBuildsFromState(State, _) :- State = [].
+getBuildsFromState(State, Builds) :- nth0(8, State, Builds).
+
+getTableCardsFromState(State, _) :- State = [].
+getTableCardsFromState(State, TableCards) :- nth0(9, State, TableCards).
+
+getPlayNextFromState(State, _) :- State = [].
+getPlayNextFromState(State, NewNextPlayer) :- nth0(10, State, NewNextPlayer).
+
+selectCard([], _, _).
+selectCard(HumanHandBeforeMove, Card, Input) :- nth0(Input, HumanHandBeforeMove, Card).
+selectCard(ComputerHandBeforeMove, Card, Input) :- nth0(Input, ComputerHandBeforeMove, Card).
+
+trail(Card, TableCardsBeforeMove, TableCards) :- TableCards = [TableCardsBeforeMove | Card].
+
+getMove(NextPlayer, TableCardsBeforeMove, HumanHandBeforeMove, ComputerHandBeforeMove, HumanHand, ComputerHand, TableCards) :-
+                                                                                    getPlayNextFromState(State, NewNextPlayer),
+                                                                                    NextPlayer = NewNextPlayer,
+                                                                                    NextPlayer == human,
+                                                                                    write("Select the idx of card in your hand: "),
+                                                                                    printCards(HumanHandBeforeMove),
+                                                                                    read(Input),
+                                                                                    selectCard(HumanHandBeforeMove, Card, Input),
+                                                                                    trail(Card, TableCardsBeforeMove, TableCards).
+
+printWhoseTurn(NewNextPlayer) :- NewNextPlayer == human,
+                                write("Human player's turn."), nl.
+printWhoseTurn(NewNextPlayer) :- NewNextPlayer == computer,
+                                write("Computer player's turn."), nl.
+printWhoseTurn(_) :- write("Unknown Turn"), nl.                           
+
+/**
+Function Name: printBoard
+Purpose: Prints the current board using lists from State
+Parameters:
+    NewHumanHand, List containing cards in human's hand..
+    NewTableCards, List containing cards on the table.
+    NewComputerHand, List containing cards in computer's hand.
+**/
+printBoard(State, HumanPile, HumanHandBeforeMove, TableCardsBeforeMove, ComputerPile, ComputerHandBeforeMove) :- getPlayNextFromState(State, NewNextPlayer),
+                                        printWhoseTurn(NewNextPlayer),
+                                        write("Human Pile: "),
+                                        printCards(HumanPile),
+                                        nl,
+                                        write("Human Cards: "),
+                                        printCards(HumanHandBeforeMove),
+                                        nl,
+                                        write("Table Cards: "),
+                                        printCards(TableCardsBeforeMove),
+                                        nl,
+                                        write("Computer Cards: "),
+                                        printCards(ComputerHandBeforeMove),
+                                        nl,
+                                        write("Computer Pile: "),
+                                        printCards(ComputerPile),
+                                        nl.
 
 /**
 Function Name: dealHumanCards
@@ -163,15 +225,12 @@ Parameters:
     NewHumanHand, Variable that will be instantiated to list of cards dealt to Human hand.
     HNewGameDeck, Variable that will be instantiated to new deck after cards are dealt to human.
 **/
-dealHumanCards(State, GameDeck, NewHumanHand, HNewGameDeck) :- draw(CardOne, GameDeck, NewGameDeck1),
+dealHumanCards(NewGameDeck, HumanHandBeforeMove, HNewGameDeck) :- draw(CardOne, NewGameDeck, NewGameDeck1),
                                         draw(CardTwo, NewGameDeck1, NewGameDeck2),
                                         draw(CardThree, NewGameDeck2, NewGameDeck3),
                                         draw(CardFour, NewGameDeck3, NewGameDeck4),
                                         HNewGameDeck = NewGameDeck4,
-                                        NewHumanHand = [CardOne, CardTwo, CardThree, CardFour],
-                                        write("Human Cards: "),
-                                        printCards(NewHumanHand),
-                                        nl.
+                                        HumanHandBeforeMove = [CardOne, CardTwo, CardThree, CardFour].
 
 /**
 Function Name: dealComputerCards
@@ -182,15 +241,12 @@ Parameters:
     NewComputerHand, Variable that will be instantiated to list of cards dealt to Computer hand.
     CNewGameDeck, Variable that will be instantiated to new deck after cards are dealt to computer.
 **/
-dealComputerCards(State, HNewGameDeck, NewComputerHand, CNewGameDeck) :- draw(CardOne, HNewGameDeck, NewGameDeck1),
+dealComputerCards(HNewGameDeck, ComputerHandBeforeMove, CNewGameDeck) :- draw(CardOne, HNewGameDeck, NewGameDeck1),
                                         draw(CardTwo, NewGameDeck1, NewGameDeck2),
                                         draw(CardThree, NewGameDeck2, NewGameDeck3),
                                         draw(CardFour, NewGameDeck3, NewGameDeck4),
                                         CNewGameDeck = NewGameDeck4,
-                                        NewComputerHand = [CardOne, CardTwo, CardThree, CardFour],
-                                        write("Computer Cards: "),
-                                        printCards(NewComputerHand),
-                                        nl.
+                                        ComputerHandBeforeMove = [CardOne, CardTwo, CardThree, CardFour].
 
 /**
 Function Name: dealTableCards
@@ -201,26 +257,33 @@ Parameters:
     NewComputerHand, Variable that will be instantiated to list of cards dealt to Computer hand.
     TNewGameDeck, Variable that will be instantiated to new deck after cards are dealt to table.
 **/
-dealTableCards(State, CNewGameDeck, NewTableCards, TNewGameDeck) :- draw(CardOne, CNewGameDeck, NewGameDeck1),
+dealTableCards(CNewGameDeck, TableCardsBeforeMove, TNewGameDeck) :- draw(CardOne, CNewGameDeck, NewGameDeck1),
                                         draw(CardTwo, NewGameDeck1, NewGameDeck2),
                                         draw(CardThree, NewGameDeck2, NewGameDeck3),
                                         draw(CardFour, NewGameDeck3, NewGameDeck4),
                                         TNewGameDeck = NewGameDeck4,
-                                        NewTableCards = [CardOne, CardTwo, CardThree, CardFour],
-                                        write("Table Cards: "),
-                                        printCards(NewTableCards),
-                                        nl.
+                                        TableCardsBeforeMove = [CardOne, CardTwo, CardThree, CardFour].
 
 /**
 Function Name: playRound
 Purpose: Begin playing current round
 Parameters: State, List containing all variables relevant to game play.
 **/
-playRound(State) :- getDeckFromState(State, GameDeck),
-                    shuffleDeck(GameDeck),
-                    dealHumanCards(State, GameDeck, NewHumanHand, HNewGameDeck),
-                    dealComputerCards(State, HNewGameDeck, NewComputerHand, CNewGameDeck),
-                    dealTableCards(State, CNewGameDeck, NewTableCards, TNewGameDeck).
+playRound(State) :- shuffleDeck(NewGameDeck, GameDeckBeforeMove),
+                    dealHumanCards(GameDeckBeforeMove, HumanHandBeforeMove, HNewGameDeck),
+                    dealComputerCards(HNewGameDeck, ComputerHandBeforeMove, CNewGameDeck),
+                    dealTableCards(CNewGameDeck, TableCardsBeforeMove, TNewGameDeck),
+                    /** Piles not being properly accessed from state. **/
+                    getHumanPileFromState(State, HumanPile),
+                    getComputerPileFromState(State, ComputerPile),
+                    printBoard(State, HumanPile, HumanHandBeforeMove, TableCardsBeforeMove, ComputerPile, ComputerHandBeforeMove),
+                    getRoundNumFromState(State, RoundNum),
+                    getHumanScoreFromState(State, HumanScore),
+                    getComputerScoreFromState(State, ComputerScore),
+                    getBuildsFromState(State, Builds),
+                    GameDeck = TNewGameDeck,
+                    write("About to get move."),
+                    NewState = [RoundNum, GameDeck, HumanScore, HumanHand, HumanPile, ComputerScore, ComputerHand, ComputerPile, Builds, TableCards, NextPlayer].
 
 /**
 Function Name: startNewTournament
@@ -230,6 +293,38 @@ Algorithm: Create game state list and initialize necessary variables.
 startNewTournament() :- RoundNum = 0,
                         HumanScore = 0,
                         ComputerScore = 0,
-                        State = [Score, RoundNum, GameDeck, Human, HumanScore, HumanHand, HumanPile, Computer, ComputerScore, ComputerHand, ComputerPile, Builds, TableCards, PlayNext],
+                        NextPlayer = human,
+                        HumanPile = [],
+                        ComputerPile = [],
+                        Builds = [],
+                        State = [RoundNum, GameDeck, HumanScore, HumanHand, HumanPile, ComputerScore, ComputerHand, ComputerPile, Builds, TableCards, NextPlayer],
                         playRound(State).
+startNewTournament() :- write("Failed to start new tournament.").
+
+/**
+Function Name: loadGame
+Purpose: Load a saved game state
+**/
+loadGame() :- write("Placeholder.").
+
+/**
+Function Name: loadOrNew
+Purpose: Parse user input and load saved game, start new tournament, or re-prompt user if they enter invalid input.
+**/
+loadOrNew(Input) :- Input == y,
+    loadGame().
+
+loadOrNew(Input) :- Input == n,
+    startNewTournament().
+
+loadOrNew(_) :- write("Invalid input."), nl,
+                start().
+
+/**
+Function Name: start
+Purpose: Starts the game flow. Allows user to input 
+**/
+start() :- write("Would you like to load a saved game?(y/n): "),
+            read(Input),
+            loadOrNew(Input).
 
